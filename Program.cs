6a810271f43ecafd801fd501fee57af4e6c2bfcc
Cihan ÇALLI <cihan.callii@gmail.com>
2022-03-31 13:38:43 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstReverse(Console.ReadLine()));
+        }
+
+        public static string FirstReverse(string str) {
+
+            // code goes here  
+            string answer = "";
+            for(int i = str.Length-1; i>=0; i--){
+            answer += str[i];
+            }
+            return answer;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# First Reverse
+## Big-O notation O(n)
+
+* All the test case outputs were correct
+
+* This user submitted their solution in 3 minutes
+
+[coderbyte](https://coderbyte.com/profile/cihancalli)
